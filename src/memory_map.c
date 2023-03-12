46815b33feb43c7b8cfe95fc5ecf3aa02c9f1b67
Clif Wolfe <clifwolfe1@gmail.com>
2023-03-12 17:22:17 -0400

    added a minimal hexdump function

--- src/memory_map.c
+++ src/memory_map.c
@@ -18,6 +18,76 @@
 #define MODULE_NAME "memory_map"
 #define DEVICE_NAME "kmaps"
 
+void DumpHex(const void* data, size_t size) {
+	char ascii[17];
+	size_t i, j;
+	ascii[16] = '\0';
+	for (i = 0; i < size; ++i) {
+		printk(KERN_INFO "%02X ", ((unsigned char*)data)[i]);
+		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+			ascii[i % 16] = ((unsigned char*)data)[i];
+		} else {
+			ascii[i % 16] = '.';
+		}
+		if ((i+1) % 8 == 0 || i+1 == size) {
+			printk(KERN_INFO " ");
+			if ((i+1) % 16 == 0) {
+				printk(KERN_INFO "|  %s \n", ascii);
+			} else if (i+1 == size) {
+				ascii[(i+1) % 16] = '\0';
+				if ((i+1) % 16 <= 8) {
+					printk(KERN_INFO " ");
+				}
+				for (j = (i+1) % 16; j < 16; ++j) {
+					printk(KERN_INFO "   ");
+				}
+				printk(KERN_INFO "|  %s \n", ascii);
+			}
+		}
+	}
+}
+
+void dmesg_dump_hex(const void* data, size_t size) {
+    char out_buffer[128];
+    for (int i = 0; i < size; i = i + (sizeof(void*)*2)){
+        memset(out_buffer, 0, sizeof(out_buffer));
+        // TODO: maybe make this less bad
+        snprintf(out_buffer, sizeof(out_buffer),
+                "\x01%016lx: "
+                "%02x %02x %02x %02x "
+                "%02x %02x %02x %02x "
+                "%02x %02x %02x %02x "
+                "%02x %02x %02x %02x",
+                ((size_t)data) + i,
+                *(uint8_t*)((size_t)data+i+0),
+                *(uint8_t*)((size_t)data+i+1),
+                *(uint8_t*)((size_t)data+i+2),
+                *(uint8_t*)((size_t)data+i+3),
+
+                *(uint8_t*)((size_t)data+i+4),
+                *(uint8_t*)((size_t)data+i+5),
+                *(uint8_t*)((size_t)data+i+6),
+                *(uint8_t*)((size_t)data+i+7),
+
+                *(uint8_t*)((size_t)data+i+8),
+                *(uint8_t*)((size_t)data+i+9),
+                *(uint8_t*)((size_t)data+i+10),
+                *(uint8_t*)((size_t)data+i+11),
+
+                *(uint8_t*)((size_t)data+i+12),
+                *(uint8_t*)((size_t)data+i+13),
+                *(uint8_t*)((size_t)data+i+14),
+                *(uint8_t*)((size_t)data+i+15)
+                );
+        printk(out_buffer);
+
+
+
+
+    }
+
+}
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Clifton Wolfe");
 MODULE_DESCRIPTION("Memory map of the kernel");
@@ -35,7 +105,25 @@ static ssize_t memory_map_read(struct file *flip, char *buffer, size_t len, loff
     pmd_t * pmd = NULL;
     pte_t * pte = NULL;
     size_t nread = 0;
-    unsigned long addr = (unsigned long)&try_module_get;
+    size_t addr = (size_t)&try_module_get;
+    printk(KERN_INFO "try_module_get 0x%lx\n", addr);
+    size_t pgdir_mask = PGDIR_MASK;
+    printk(KERN_INFO "PGDIR_MASK 0x%lx\n", pgdir_mask);
+
+    size_t pmd_mask = PMD_MASK;
+    printk(KERN_INFO "PMD_MASK 0x%lx\n", pmd_mask);
+
+    pgd = current_mm->pgd;
+
+    printk(KERN_INFO "current_mm 0x%lx\n", (size_t)current_mm);
+    printk(KERN_INFO "current_mm base 0x%lx\n", (size_t)current_mm->mmap_base);
+
+    printk(KERN_INFO "pgd ptr 0x%lx\n", (size_t)pgd);
+    printk(KERN_INFO "pgd 0x%lx\n", *(size_t*)pgd);
+
+    //DumpHex(pgd, 256);
+    dmesg_dump_hex(addr, 256);
+
 
     return nread;
 }
@@ -51,6 +139,7 @@ static int memory_map_open(struct inode *inode, struct file *file) {
     if (atomic_read(&memory_map_open_count)) {
         return -EBUSY;
     }
+    printk(KERN_INFO "opening device\n");
     atomic_inc(&memory_map_open_count);
     current_mm = current->mm;
     try_module_get(THIS_MODULE);
@@ -76,11 +165,14 @@ static struct file_operations file_ops = {
 static int __init memory_map_init(void)
 {
     printk(KERN_INFO "Starting kernel module!\n");
+    // TODO: real major num here
     int res = register_chrdev(0, DEVICE_NAME, &file_ops);
     if (res < 0) {
         printk(KERN_WARNING "Failed to start kernel module %d\n", res);
         return res;
     }
+    printk(KERN_INFO "Major number %d\n", res);
+
     major_num = res;
     return 0;
 }
